cf757ed03929d9511cc0a953777e4467c207b630
logerror <freedom521fd@gmail.com>
2025-04-01 17:32:42 +0800

    optimize

--- internal/server/web/static/js/terminal.js
+++ internal/server/web/static/js/terminal.js
@@ -9,16 +9,38 @@ if (!hostTag || !clientId) {
     const term = new Terminal({
         cursorBlink: true,
         convertEol: true,
+        fontSize: 16,
+        lineHeight: 1.2,
+        fontFamily: "'Inter', monospace",
+        letterSpacing: 0.5,
+        allowProposedGlyphs: true,
+        scrollback: 5000,
         theme: {
             background: '#1e1e1e',
-            foreground: '#ffffff'
-        }
+            foreground: '#d4d4d4',
+            cursor: '#aeafad',
+            selection: 'rgba(255, 255, 255, 0.2)',
+            black: '#1e1e1e',
+            red: '#f44747',
+            green: '#6a9955',
+            yellow: '#d7ba7d',
+            blue: '#569cd6',
+            magenta: '#646695',
+            cyan: '#4EC9B0',
+            white: '#d4d4d4'
+        },
+        smoothScrollDuration: 80,
+        disableStdin: false,
+        allowTransparency: false,
+        overviewRulerTop: 15,
+        screenReaderMode: false,
+        disableScrollToInput: true
     });
 
     const fitAddon = new FitAddon.FitAddon();
     term.loadAddon(fitAddon);
 
-    const container = document.getElementById('terminal-container');
+    const container = document.getElementById('terminal-t2t');
     term.open(container);
     fitAddon.fit();
     term.focus();
@@ -32,8 +54,7 @@ if (!hostTag || !clientId) {
 
     helpUrl = `https://${window.location.host}/help`;
     socket.onopen = () => {
-        term.write("\x1b[32mConnected to terminal \x1b[0m\r\n");
-        term.write(`\x1b[32mFor more information: ${helpUrl} \x1b[0m\r`);
+        term.write(`\x1b[32mConnected to ${hostTag}:${clientId} terminal. For more information: ${helpUrl} \x1b[0m\r`);
         // 发送初始终端大小
         const dims = fitAddon.proposeDimensions();
         if (dims) {
